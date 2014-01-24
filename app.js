72b4069875abc6cf276ad45c172c0421b7373130
Bruno Sanches <brunopsanches@gmail.com>
2014-01-23 23:31:08 -0800

    Pretend server is slow to see loading icon haha

--- app.js
+++ app.js
@@ -32,14 +32,18 @@ app.get('/send-money', function(req, res) {
 
 // Validate Email
 app.post('/validate-email', function(req, res) {
-  if (!req.body.hasOwnProperty('email')) {
-    res.statusCode = 400;
-    return res.send('Error 400: Incorrect POST syntax.');
-  }
+  // node is too fast, pretend server is slow to see loading icon lol :P
+  setTimeout(function() {
+    if (!req.body.hasOwnProperty('email')) {
+      res.statusCode = 400;
+      return res.send('Error 400: Incorrect POST syntax.');
+    }
 
-  res.json({
-    valid: validator.isEmail(req.body.email)
-  });
+    res.json({
+      valid: validator.isEmail(req.body.email)
+    });
+
+  }, 1000);
 });
 
 app.listen(3000);
