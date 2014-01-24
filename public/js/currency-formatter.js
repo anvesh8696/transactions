6d5658232222c3a99d3ee117be6923564583df91
Bruno Sanches <brunopsanches@gmail.com>
2014-01-24 01:36:25 -0800

    Add comma separated values for thousands

--- public/js/currency-formatter.js
+++ public/js/currency-formatter.js
@@ -8,6 +8,12 @@ var moneyFormatter = {
     number = number.length ? number : '0';
     precision = precision.length === 1 ? '0' + precision : precision;
 
+    // add commas for thousands
+    // kind of black magic
+    number = number.split('').reverse().join('') // reverse string
+             .replace(/(\d{3})(?=\d)/g, "$1,")   // add the commas
+             .split('').reverse().join('');      // reverse it back
+
     return '$' + number + '.' + precision;
   }
 };
@@ -19,11 +25,12 @@ function assert(truthiness) {
   }
 }
 
-console.log(moneyFormatter.format(0))
+console.log(moneyFormatter.format(100000))
 
 // Test 1
 assert(moneyFormatter.format(0) === '$0.00');
 assert(moneyFormatter.format(1) === '$0.01');
 assert(moneyFormatter.format(10) === '$0.10');
+assert(moneyFormatter.format(100000) === '$1,000.00');
 
 console.log('success!');
