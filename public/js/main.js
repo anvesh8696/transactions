6bf68127a667ba364ce3f2eaddd36496f536de44
Bruno Sanches <brunopsanches@gmail.com>
2014-01-23 23:23:03 -0800

    Implement dynamic email validation
    * Trigger email validation on the server after the email input field value was changed and idle for a second
    * Prevent multiple validation resquests from being triggered with the SendMoney.isEmailBeingValidate semaphore variable
    * Display loading / valid / invalid email status
    * Refactor some jQuery selectors - adding it to SendMoney object to avoid keep selecting the DOM all the time

--- public/js/main.js
+++ public/js/main.js
@@ -4,16 +4,86 @@
   // namespace for better semantics
   var SendMoney = {
     init: function() {
+      this.emailWrapper = $('.form-item.to');
+      this.emailLoader  = this.emailWrapper.find('.loader');
+      this.paymentItems = $('.payment-item');
 
       SendMoney.bindEvents();
     },
 
+    // Semaphore variable to avoid multiple server requests
+    isEmailBeingValidate: false,
+
     bindEvents: function() {
+      SendMoney.bindValidations();
       SendMoney.bindPaymentForSelection();
       SendMoney.bindFriendlySelection();
 
       // Buttons
       SendMoney.bindClearButton();
+    },
+
+    bindValidations: function() {
+      SendMoney.bindEmailValidation();
+      // SendMoney.bindAmountValidation();
+    },
+
+    bindEmailValidation: function() {
+      var emailInput = $('#to'),
+
+          lastValue = emailInput.val(),
+
+          // UX - trigger validation after some determined time
+          triggerValidationTimeout;
+
+      emailInput.on('keyup', function(e) {
+        if (lastValue !== emailInput.val()) {
+          lastValue = emailInput.val();
+
+          clearTimeout(triggerValidationTimeout);
+
+          setTimeout(function() {
+            SendMoney.validateEmail(lastValue);
+          }, 1000);
+        }
+      });
+    },
+
+    validateEmail: function(email) {
+      if (!SendMoney.isEmailBeingValidate) {
+        SendMoney.showEmailLoading();
+        SendMoney.isEmailBeingValidate = true;
+
+        $.post('/validate-email', { email: email }, function(response) {
+          SendMoney.hideEmailLoading();
+
+          if (response.valid) {
+            SendMoney.validEmailDisplay();
+          }
+          else {
+            SendMoney.invalidEmailDisplay();
+          }
+
+          SendMoney.isEmailBeingValidate = false;
+        });
+      }
+    },
+
+    showEmailLoading: function() {
+      SendMoney.emailWrapper.removeClass('invalid').removeClass('valid');
+      SendMoney.emailLoader.show();
+    },
+
+    hideEmailLoading: function() {
+      SendMoney.emailLoader.hide();
+    },
+
+    validEmailDisplay: function() {
+      SendMoney.emailWrapper.removeClass('invalid').addClass('valid');
+    },
+
+    invalidEmailDisplay: function() {
+      SendMoney.emailWrapper.removeClass('valid').addClass('invalid');
     },
 
     bindFriendlySelection: function() {
@@ -25,16 +95,14 @@
     },
 
     bindPaymentForSelection: function() {
-      var paymentFor = $('.payment-for');
-
-      paymentFor.on('click', '.payment-item', function(e) {
+      $('.payment-for').on('click', '.payment-item', function(e) {
         SendMoney.deactivateAllPaymentItems();
         $(this).addClass('active');
       });
     },
 
     deactivateAllPaymentItems: function() {
-      $('.payment-item').removeClass('active');
+      SendMoney.paymentItems.removeClass('active');
     },
 
     bindClearButton: function() {
