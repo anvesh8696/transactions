8a580e7451d31ab066aa47de0d7427c2263adc5b
Bruno Sanches <brunopsanches@gmail.com>
2014-01-24 01:20:12 -0800

    Fixed precision to 2

--- public/js/currency-formatter.js
+++ public/js/currency-formatter.js
@@ -1,6 +1,14 @@
 var moneyFormatter = {
   format: function(value) {
-    return "$0.0" + value;
+    var digits = value.toString().split(''),
+        number = digits.slice(0, -2).join(''),
+        precision = digits.slice(-2).join('');
+
+    // fix minimum spots
+    number = number.length ? number : '0';
+    precision = precision.length === 1 ? '0' + precision : precision;
+
+    return '$' + number + '.' + precision;
   }
 };
 
@@ -11,8 +19,11 @@ function assert(truthiness) {
   }
 }
 
+console.log(moneyFormatter.format(0))
+
 // Test 1
 assert(moneyFormatter.format(0) === '$0.00');
 assert(moneyFormatter.format(1) === '$0.01');
+assert(moneyFormatter.format(10) === '$0.10');
 
 console.log('success!');
