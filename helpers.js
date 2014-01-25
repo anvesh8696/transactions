72eb9d79cc817a071684b9bd9573681538cbd811
Bruno Sanches <brunopsanches@gmail.com>
2014-01-24 17:53:50 -0800

    Create helpers module to return current user (mocked - should be from a session)

---
+++ helpers.js
@@ -0,0 +1,15 @@
+module.exports = (function() {
+
+  return {
+    currentUser: function() {
+      // mocked data
+      return {
+        id: 1,
+        name: "John Lennon",
+        email: "john@example.com",
+        createdAt: Date.now()
+      }
+    }
+  }
+
+})();
