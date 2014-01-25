f122e7a17bc29356d4a684d76c53fe0025997365
Bruno Sanches <brunopsanches@gmail.com>
2014-01-24 19:31:14 -0800

    Add formatMoney to helpers

--- helpers.js
+++ helpers.js
@@ -1,4 +1,9 @@
 module.exports = (function() {
+  var currencyPrefixes = {
+    'USD': '$',
+    'EUR': '€',
+    'JPY': '¥'
+  };
 
   return {
     currentUser: function() {
@@ -9,6 +14,29 @@ module.exports = (function() {
         email: "john@example.com",
         createdAt: Date.now()
       }
+    },
+
+    formatMoney: function(value, currency) {
+      // remove previous formatting
+      value = value.toString().replace(/[^0-9]/g, '');
+      // empty string defaults to 0
+      value = value || "0";
+
+      var digits = value.split(''),
+          number = digits.slice(0, -2).join(''),
+          precision = digits.slice(-2).join('');
+
+      // fix minimum number size
+      number = number.length ? number.replace(/^0*([0-9]+)$/, "$1") : '0';
+      precision = precision.length === 1 ? '0' + precision : precision;
+
+      // add commas for thousands
+      // kind of black magic
+      number = number.split('').reverse().join('') // reverse string
+               .replace(/(\d{3})(?=\d)/g, "$1,")   // add the commas
+               .split('').reverse().join('');      // reverse it back
+
+      return currencyPrefixes[currency] + number + '.' + precision;
     }
   };
 
